47be4e1b53ee496b915c6a0fc687132377e81e9c
张一鸣 <2283942163@qq.com>
2017-02-05 17:17:53 +0800

    quick sort ，没有写快速选择

--- sort/sort.c
+++ sort/sort.c
@@ -8,14 +8,15 @@
 
 typedef int ElementType;
 
-void Swap(ElementType *Lhs,ElementType *Rhs){
-  ElementType  Tmp = *Lhs;
-    *Lhs = *Rhs;
-    *Rhs = Tmp;
+void Swap (ElementType *Lhs, ElementType *Rhs)
+{
+  ElementType Tmp = *Lhs;
+  *Lhs = *Rhs;
+  *Rhs = Tmp;
 }
 
 void
-InsertionSort( ElementType A[ ], int N )
+InsertionSort (ElementType A[], int N)
 {
   int j, P;
   ElementType Tmp;
@@ -30,27 +31,27 @@ InsertionSort( ElementType A[ ], int N )
   for (P = 1; P < N; P++)
     {
       Tmp = A[P];
-      for ( j = P; j > 0 && A[ j - 1] > Tmp; j--)
+      for (j = P; j > 0 && A[j - 1] > Tmp; j--)
         {
-          A[ j ] = A [ j - 1];
+          A[j] = A[j - 1];
         }
-      A [ j ] = Tmp;
+      A[j] = Tmp;
     }
 }
-void Shellsort(ElementType A[], int N)
+void Shellsort (ElementType A[], int N)
 {
-  int i , j , Increment;
+  int i, j, Increment;
   ElementType Tmp;
 
-  for (Increment = N / 2; Increment > 0 ; Increment /= 2)
+  for (Increment = N / 2; Increment > 0; Increment /= 2)
     {
       for (int i = Increment; i < N; ++i)
         {
           Tmp = A[i];
-          for ( j = i; j >= Increment; j -= Increment)
+          for (j = i; j >= Increment; j -= Increment)
             {
-              if ( Tmp < A [ j - Increment ])
-                A[j] = A [ j - Increment];
+              if (Tmp < A[j - Increment])
+                A[j] = A[j - Increment];
               else
                 break;
             }
@@ -61,16 +62,16 @@ void Shellsort(ElementType A[], int N)
 
 #define LeftChild(i) ( 2 * (i) + 1)
 
-void PercDown (ElementType A[ ], int i, int N)
+void PercDown (ElementType A[], int i, int N)
 {
 
   int Child;
-  ElementType  Tmp;
+  ElementType Tmp;
 
-  for ( Tmp = A[i] ; LeftChild ( i ) < N; i = Child)
+  for (Tmp = A[i]; LeftChild (i) < N; i = Child)
     {
       Child = LeftChild (i);
-      if (Child != N-1 && A[Child + 1] > A[Child])
+      if (Child != N - 1 && A[Child + 1] > A[Child])
         Child++;
       if (Tmp < A[Child])
         A[i] = A[Child];
@@ -80,27 +81,29 @@ void PercDown (ElementType A[ ], int i, int N)
   A[i] = Tmp;
 }
 
-void Heapsort (ElementType A[], int N) {
+void Heapsort (ElementType A[], int N)
+{
   int i;
 
-  for ( i = N / 2; i >= 0; i--)
+  for (i = N / 2; i >= 0; i--)
     {
-      PercDown (A,i,N);
+      PercDown (A, i, N);
     }
-  for ( i = N - 1; i > 0 ; i-- )
+  for (i = N - 1; i > 0; i--)
     {
       //将最大的放在最后
-      Swap (&A[ 0 ],&A[ i ]);
+      Swap (&A[0], &A[i]);
       // 将剩余的排序最大的上去
-      PercDown (A,0,i);
+      PercDown (A, 0, i);
     }
 }
 
 /* Lpos = start of left half, Rpos = start of right half */
-void Merge( ElementType A[ ], ElementType TmpArray[ ],
-       int Lpos, int Rpos, int RightEnd ) {
+void Merge (ElementType A[], ElementType TmpArray[],
+            int Lpos, int Rpos, int RightEnd)
+{
 
-  printf ("Lpos :%d Rpos:%d RightEnd:%d\n",Lpos,Rpos,RightEnd);
+  printf ("Lpos :%d Rpos:%d RightEnd:%d\n", Lpos, Rpos, RightEnd);
 //  int i, LeftEnd, NumElements, TmpPos;
 //
 //  LeftEnd = Rpos - 1;
@@ -123,31 +126,28 @@ void Merge( ElementType A[ ], ElementType TmpArray[ ],
 //  for( i = 0; i < NumElements; i++, RightEnd-- )
 //    A[ RightEnd ] = TmpArray[ RightEnd ];
 
-  int i , LeftEnd, NumElements,TmpPos;
+  int i, LeftEnd, NumElements, TmpPos;
 
   LeftEnd = Rpos - 1;
   TmpPos = Lpos;
   NumElements = RightEnd - Lpos + 1;
 
-  while ( Lpos <= LeftEnd && Rpos <= RightEnd)
-    if ( A [Lpos] <= A[Rpos])
-      TmpArray[ TmpPos++ ] = A[ Lpos++ ];
+  while (Lpos <= LeftEnd && Rpos <= RightEnd)
+    if (A[Lpos] <= A[Rpos])
+      TmpArray[TmpPos++] = A[Lpos++];
     else
-      TmpArray[ TmpPos++ ] = A[ Rpos++ ];
+      TmpArray[TmpPos++] = A[Rpos++];
 
-  while ( Lpos <= LeftEnd )
-    TmpArray[ TmpPos++ ] = A[ Lpos++ ];
-  while ( Rpos <= RightEnd)
-    TmpArray[ TmpPos++ ] = A[ Rpos++ ];
-
+  while (Lpos <= LeftEnd)
+    TmpArray[TmpPos++] = A[Lpos++];
+  while (Rpos <= RightEnd)
+    TmpArray[TmpPos++] = A[Rpos++];
 
 }
 
-
-
 void
-MSort( ElementType A[ ], ElementType TmpArray[ ],
-       int Left, int Right )
+MSort (ElementType A[], ElementType TmpArray[],
+       int Left, int Right)
 {
 //  int Center;
 //
@@ -160,17 +160,17 @@ MSort( ElementType A[ ], ElementType TmpArray[ ],
 //    }
 
   int Center;
-  if ( Left < Right )
+  if (Left < Right)
     {
-      Center = ( Left + Right ) / 2;
-      MSort ( A, TmpArray ,Left, Center);
-      MSort ( A, TmpArray, Center + 1, Right);
-      Merge ( A, TmpArray, Left, Center + 1,Right);
+      Center = (Left + Right) / 2;
+      MSort (A, TmpArray, Left, Center);
+      MSort (A, TmpArray, Center + 1, Right);
+      Merge (A, TmpArray, Left, Center + 1, Right);
     }
 }
 
 void
-Mergesort( ElementType A[ ], int N )
+Mergesort (ElementType A[], int N)
 {
 //  ElementType *TmpArray;
 //
@@ -184,56 +184,116 @@ Mergesort( ElementType A[ ], int N )
 //    FatalError( "No space for tmp array!!!" );
   ElementType *TmpArray;
 
-  TmpArray = malloc ( N * sizeof ( ElementType ));
+  TmpArray = malloc (N * sizeof (ElementType));
   if (TmpArray != NULL)
     {
-      MSort (A, TmpArray ,0, N-1);
+      MSort (A, TmpArray, 0, N - 1);
       free (TmpArray);
     }
   else
-      FatalError( "No space for tmp array!!!" );
+    FatalError("No space for tmp array!!!");
 
 }
 
+ElementType
+Median3 (ElementType A[], int Left, int Right)
+{
+  int Center = (Left + Right) / 2;
+  if (A[Left] > A[Center])
+    Swap (&A[Left], &A[Center]);
+  if (A[Left] > A[Right])
+    Swap (&A[Left], &A[Right]);
+  if (A[Center] > A[Right])
+    Swap (&A[Center], &A[Right]);
 
+  //这里是因为right位置伤的数一定是比中值大的，所以就不要加入排序
+  Swap (&A[Center], &A[Right - 1]);
 
-void Permute (ElementType A[],int N) {
+  return A[Right - 1];
+
+}
+
+#define Cutoff ( 3 )
+
+void
+Qsort (ElementType A[], int Left, int Right)
+{
+
+  int i, j;
+  ElementType Pivot;
+
+  if (Left + Cutoff <= Right)
+    {
+      Pivot = Median3 (A, Left, Right);
+      i = Left;
+      j = Right - 1;
+      for (;;)
+        {
+          while (A[++i] < Pivot)
+            {}
+          while (A[--j] > Pivot)
+            {}
+          if (i < j)
+            Swap (&A[i], &A[j]);
+          else
+            break;
+        }
+      Swap (&A[i],&A[Right-1]);
+      Qsort (A,Left,i-1);
+      Qsort (A,i+1,Right);
+    }
+  else
+    InsertionSort (A + Left, Right - Left + 1);
+
+}
+
+void Quicksort (ElementType A[], int N)
+{
+  Qsort (A, 0, N - 1);
+}
+
+void Permute (ElementType A[], int N)
+{
   int i;
 
   for (i = 0; i < N; i++)
     A[i] = i;
   for (i = 1; i < N; i++)
-    Swap (&A[i],&A[rand() % ( i + 1)]);
+    Swap (&A[i], &A[rand () % (i + 1)]);
 }
 
-void
-Checksort( ElementType A[ ], int N )
+void Checksort (ElementType A[], int N)
 {
   int i;
-  for( i = 0; i < N; i++ )
-    if( A[ i ] != i )
-      printf( "Sort fails: %d %d\n", i, A[ i ] );
-  printf( "Check completed\n" );
+  for (i = 0; i < N; i++)
+    if (A[i] != i)
+      printf ("Sort fails: %d %d\n", i, A[i]);
+  printf ("Check completed\n");
+
 }
 
-void Copy(ElementType Lhs[], const ElementType Rhs[], int N) {
+void Copy (ElementType Lhs[], const ElementType Rhs[], int N)
+{
   int i;
   for (i = 0; i < N; ++i)
     {
-      Lhs[ i ] = Rhs[ i ];
+      Lhs[i] = Rhs[i];
     }
 }
 
-#define MaxSize 7000
-int Arr1[ MaxSize ];
-int Arr2[ MaxSize ];
 
-void testSort() {
+#define MaxSize 7000
+int Arr1[MaxSize];
+int Arr2[MaxSize];
+
+void testSort ()
+{
+
   int i;
 
 //  for( i = 0; i < 10; i++ )
 //    {
-      Permute( Arr2, MaxSize );
+  Permute (Arr2, MaxSize);
 //      Copy( Arr1, Arr2, MaxSize );
 //      InsertionSort( Arr1, MaxSize );
 //      Checksort( Arr1, MaxSize );
@@ -246,13 +306,13 @@ void testSort() {
 //      Heapsort( Arr1, MaxSize );
 //      Checksort( Arr1, MaxSize );
 
-      Copy( Arr1, Arr2, MaxSize );
-      Mergesort( Arr1, MaxSize );
-      Checksort( Arr1, MaxSize );
+//  Copy (Arr1, Arr2, MaxSize);
+//  Mergesort (Arr1, MaxSize);
+//  Checksort (Arr1, MaxSize);
 //
-//      Copy( Arr1, Arr2, MaxSize );
-//      Quicksort( Arr1, MaxSize );
-//      Checksort( Arr1, MaxSize );
+      Copy( Arr1, Arr2, MaxSize );
+      Quicksort( Arr1, MaxSize );
+      Checksort( Arr1, MaxSize );
 //
 //      Copy( Arr1, Arr2, MaxSize );
 //      Qselect( Arr1, MaxSize / 2 + 1 + i, 0, MaxSize - 1 );
